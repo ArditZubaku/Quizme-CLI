67f348362cca174e6e64e870d6fcc64e405d4d15
Ardit Zubaku <zubaku92@gmail.com>
2023-12-12 16:33:22 +0100

    Add question/answer objects to data.json file.

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,14 @@ interface Data {
   id: number;
   question: string;
   answer: string;
-  lastAnsweredCorrect: boolean;
-  lastAsked: string;
+  lastAnsweredCorrect?: boolean;
+  lastAsked?: string;
 }
 
-type PromptNames = "useranswer";
+type PromptNames = "userAnswer" | "targetQuestion" | "targetAnswer";
 
 interface Prompt {
-  type: string;
+  type: "input";
   name: PromptNames | string;
   message: string;
 }
@@ -37,28 +37,41 @@ if (flags.includes("a") || flags.includes("add")) {
   askQuestion();
 }
 
-async function addQuestion() {
-  const answers = await inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?",
-    },
-    {
-      type: "input",
-      name: "live",
-      message: "Where do you live?",
-    },
-    {
-      type: "list",
-      name: "live2",
-      message: "Where do you live?",
-      choices: ["NI", "Wales", "Scotland", "England", "Elsewhere"],
-    },
-  ]);
+async function addQuestion(): Promise<void> {
+  // - Start a dialogue
+  console.log("Hello, let's add a new question!");
 
-  console.log(`Your name is ${answers.name}.`);
-  console.log(`You live in ${answers.live} which is in ${answers.live2}`);
+  // - Ask me for a question
+  const prompt1: Prompt = {
+    type: "input",
+    name: "targetQuestion",
+    message: "What is your question?",
+  };
+
+  // - Ask me for the answer
+  const prompt2: Prompt = {
+    type: "input",
+    name: "targetAnswer",
+    message: "What is your answer?",
+  };
+
+  const responses: Answers = await inquirer.prompt([prompt1, prompt2]);
+
+  console.log(responses);
+
+  // - Store for future uses
+  const data: Buffer = await fs.readFile(FILE_PATH);
+  const parsedData: Data[] = JSON.parse(data.toString());
+
+  parsedData.push({
+    id: getID(parsedData),
+    question: responses.targetQuestion,
+    answer: responses.targetAnswer,
+  });
+
+  // TODO: Optimize this
+  await fs.truncate(FILE_PATH);
+  await fs.appendFile(FILE_PATH, JSON.stringify(parsedData));
 }
 
 async function askQuestion(): Promise<void> {
@@ -79,7 +92,7 @@ async function askQuestion(): Promise<void> {
 
   const answers: Answers = await inquirer.prompt([prompt]);
 
-  target.lastAnsweredCorrect = checkAnswer(answers.useranswer, answer);
+  target.lastAnsweredCorrect = checkAnswer(answers.userAnswer, answer);
   target.lastAsked = new Date().toUTCString();
 
   const newData: Data[] = parsedData.filter((item) => item.id !== target.id);
@@ -98,3 +111,33 @@ function checkAnswer(input: string, answer: string): boolean {
     return false;
   }
 }
+
+function getID(data: Data[]) {
+  return Math.max(...data.map((item) => item.id)) + 1;
+}
+
+/*
+async function test() {
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "input",
+      name: "live",
+      message: "Where do you live?",
+    },
+    {
+      type: "list",
+      name: "live2",
+      message: "Where do you live?",
+      choices: ["NI", "Wales", "Scotland", "England", "Kosovo", "Elsewhere"],
+    },
+  ]);
+
+  console.log(`Your name is ${answers.name}.`);
+  console.log(`You live in ${answers.live} which is in ${answers.live2}`);
+}
+*/
