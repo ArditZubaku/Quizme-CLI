b15e73cbe5076895bf6a24735104184cd9dbfd61
Ardit Zubaku <zubaku92@gmail.com>
2023-12-12 08:18:27 +0100

    Parse user input with readline.

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,15 @@
-"use strict";
-console.log(process.argv);
+import readline from "node:readline/promises";
+const rl = readline.createInterface({
+    terminal: true,
+    input: process.stdin,
+    output: process.stdout,
+});
+console.log("What is your name?");
+const answer = await rl.question("What is your name?");
+console.log(`Your name is ${answer}`);
+const answer2 = await rl.question("Where do you live?");
+console.log(`Your location is ${answer2}`);
+rl.close();
 const flags = [];
 process.argv.forEach((arg) => {
     if (arg.startsWith("--") || arg.startsWith("-")) {
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,19 @@
-console.log(process.argv);
+import readline from "node:readline/promises";
+
+// Duplex stream
+const rl = readline.createInterface({
+  terminal: true,
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log("What is your name?");
+const answer: string = await rl.question("What is your name?");
+console.log(`Your name is ${answer}`);
+const answer2: string = await rl.question("Where do you live?");
+console.log(`Your location is ${answer2}`);
+
+rl.close();
 
 const flags: string[] = [];
 
